b74e654eb7653776eb8921e30e1c02e64df00a12
Mark <56490702+marcosoft47@users.noreply.github.com>
2025-09-26 11:23:42 -0300

    simplified thread listener

--- trabalho1/Client.java
+++ trabalho1/Client.java
@@ -5,12 +5,12 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
-
 public class Client {
+
     private String hostname;
     private int port;
-    private String username;
     private Socket socket;
+    private DataInputStream dataIn;
     private DataOutputStream dataOut;
 
     public Client(String hostname, int port) {
@@ -23,13 +23,14 @@ public class Client {
             socket = new Socket(hostname, port);
             System.out.println("Conectado ao servidor de chat.");
 
-            new Thread(new ServerListener(socket, this)).start();
-            
+            dataIn = new DataInputStream(socket.getInputStream());
             dataOut = new DataOutputStream(socket.getOutputStream());
+
+            handleLogin();
+            new Thread(new ServerListener(socket)).start();
             
             Scanner scanner = new Scanner(System.in);
             String text;
-
             do {
                 text = scanner.nextLine();
                 handleUserInput(text);
@@ -41,7 +42,29 @@ public class Client {
         } catch (UnknownHostException ex) {
             System.out.println("Servidor não encontrado: " + ex.getMessage());
         } catch (IOException ex) {
-            System.out.println("Erro de I/O: " + ex.getMessage());
+            System.out.println("Erro de I/O ou conexão perdida: " + ex.getMessage());
+        }
+    }
+
+    private void handleLogin() throws IOException {
+        Scanner consoleScanner = new Scanner(System.in);
+        String serverResponse;
+        
+        while (true) {
+            serverResponse = dataIn.readUTF();
+
+            if (serverResponse.equals("SUBMIT_USERNAME")) {
+                System.out.print("Digite seu nome de usuário: ");
+                String username = consoleScanner.nextLine();
+                dataOut.writeUTF(username);
+                dataOut.flush();
+            } else if (serverResponse.equals("USERNAME_TAKEN")) {
+                System.out.println("Nome de usuário já em uso. Tente outro.");
+            } else if (serverResponse.equals("USERNAME_ACCEPTED")) {
+                System.out.println("Nome de usuário aceito. Você está online!");
+                System.out.println("Comandos disponíveis: \n /users \n /send message <destinatario> <mensagem> \n /send file <destinatario> <caminho_do_arquivo> \n /help \n /sair");
+                break;
+            }
         }
     }
     
@@ -80,14 +103,6 @@ public class Client {
         }
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
     public static void main(String[] args) {
         String hostname = "localhost"; 
         int port = 12345;
@@ -99,12 +114,10 @@ public class Client {
 
 class ServerListener implements Runnable {
     private Socket socket;
-    private Client client;
     private DataInputStream dataIn;
 
-    public ServerListener(Socket socket, Client client) {
+    public ServerListener(Socket socket) {
         this.socket = socket;
-        this.client = client;
         try {
             this.dataIn = new DataInputStream(socket.getInputStream());
         } catch (IOException e) {
@@ -113,25 +126,11 @@ class ServerListener implements Runnable {
     }
 
     public void run() {
-        try (Scanner scanner = new Scanner(System.in)) {
-             while (true) {
+        try {
+            while (true) {
                 String serverResponse = dataIn.readUTF();
                 
-                if (serverResponse == null) break;
-
-                if (serverResponse.equals("SUBMIT_USERNAME")) {
-                    System.out.print("Digite seu nome de usuário: ");
-                    String username = scanner.nextLine();
-                    client.setUsername(username);
-                    DataOutputStream dataOut = new DataOutputStream(socket.getOutputStream());
-                    dataOut.writeUTF(username);
-                    dataOut.flush();
-                } else if (serverResponse.equals("USERNAME_TAKEN")) {
-                    System.out.println("Nome de usuário já em uso. Tente outro.");
-                } else if (serverResponse.equals("USERNAME_ACCEPTED")) {
-                    System.out.println("Nome de usuário aceito. Você está online!");
-                    System.out.println("Comandos disponíveis: \n /users \n /send message <destinatario> <mensagem> \n /send file <destinatario> <caminho_do_arquivo> \n /help \n /sair");
-                } else if (serverResponse.startsWith("MSG:")) {
+                if (serverResponse.startsWith("MSG:")) {
                     System.out.println(serverResponse.substring(4));
                 } else if (serverResponse.startsWith("FILE:")) {
                     handleFileReception(serverResponse);
@@ -140,9 +139,9 @@ class ServerListener implements Runnable {
                 }
             }
         } catch (EOFException e) {
-            System.out.println("Conexão com o servidor perdida.");
+            System.out.println("\nVocê foi desconectado do servidor.");
         } catch (IOException e) {
-             System.out.println("Erro ao ler do servidor: " + e.getMessage());
+            System.out.println("\nConexão com o servidor foi perdida.");
         }
     }
     
